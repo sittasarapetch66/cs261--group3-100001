754f48ba7d4987eaa3ad82a5b380d7031397f013
Petchon <petchsony123@gmail.com>
2024-11-08 17:43:36 +0700

    Version 1.1.1 Merge history and mylog to request2.0

--- public/js/webscript2.js
+++ public/js/webscript2.js
@@ -1,7 +1,7 @@
 
 
 function startFunction(){
-
+  document.getElementById("homesection").style.display = "block";
 }
 
 
@@ -19,4 +19,34 @@ function hideMenu(){
   variable.style.display = "none";
   variable2 = document.getElementById("overlaysection");
   variable2.style.display = "none";
+}
+
+function showMyListSection(){
+
+  document.getElementById("menusection").style.display = "none";
+  document.getElementById("overlaysection").style.display = "none";
+  document.getElementById("homesection").style.display = "none";
+  document.getElementById("mylistsection").style.display = "block";
+  document.getElementById("trackhistorysection").style.display = "none";
+}
+
+function showHomeSection(){
+  document.getElementById("menusection").style.display = "none";
+  document.getElementById("overlaysection").style.display = "none";
+  document.getElementById("homesection").style.display = "block";
+  document.getElementById("mylistsection").style.display = "none";
+  document.getElementById("trackhistorysection").style.display = "none";
+}
+
+function showHistorySection(){
+  document.getElementById("menusection").style.display = "none";
+  document.getElementById("overlaysection").style.display = "none";
+  document.getElementById("homesection").style.display = "none";
+  document.getElementById("mylistsection").style.display = "none";
+  document.getElementById("trackhistorysection").style.display = "block";
+}
+
+
+function toggleSection(number){
+
 }
